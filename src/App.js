25f6720b948a9d46ca67ec2311c8e2bb96fa135e
Yathaartha <maharjanyathaartha017@gmail.com>
2021-04-25 16:47:58 +0545

    added header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import PageWrapper from "./components/PageWrapper.js";
-import Home from "./components/pages/Home.js";
+import PageWrapper from "./components/PageWrapper";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+// Pages
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 class App extends Component {
   render() {
     return (
       <Router>
         <PageWrapper>
           <Route path="/" component={Home} exact={true} />
+          <Route path="/about" component={About} />
         </PageWrapper>
       </Router>
     );
