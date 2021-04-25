547fd7faa8da3e5938a10274e512b202b29b4041
Yathaartha <maharjanyathaartha017@gmail.com>
2021-04-25 14:59:36 +0545

    added routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import PageWrapper from "./components/PageWrapper.js";
 import Home from "./components/pages/Home.js";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <PageWrapper>
-        <Home />
-      </PageWrapper>
+      <Router>
+        <PageWrapper>
+          <Route path="/" component={Home} exact={true} />
+        </PageWrapper>
+      </Router>
     );
   }
 }
